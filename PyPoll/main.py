078e0ede782944858e370f906af9df959c31f189
laquita44 <quitamack@yahoo.com>
2021-11-28 00:58:51 -0800

    upload out_pypoll.txt

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -41,7 +41,8 @@ with open(file_path) as csvfile:
         holder_two = format((choice[1][1])*100/(sum(candidate_count.values())),'.3f')
         holder_three = format((choice[2][1])*100/(sum(candidate_count.values())),'.3f')
         holder_four = format((choice[3][1])*100/(sum(candidate_count.values())),'.3f')
-          
+
+
       
 #   Print values, sum, and percentages position
 print("Election Results")
@@ -57,17 +58,38 @@ print(f"Winner:  {candidate_selection[0][0][0]}")
 print("-------------------------")
 
 # -->>  Export results to text file 
+election_file = os.path.join("Output", "out_pypoll.txt")
+with open(election_file, "w") as txt_file:
+
+    txt_file.write("Election Results\n")
+    txt_file.write("-------------------------\n")
+    txt_file.write(f"Total Votes:  {sum(candidate_count.values())}\n")
+    txt_file.write("-------------------------\n")
+    txt_file.write(f"{candidate_selection[0][0][0]}: {holder_one}% ({candidate_selection[0][0][1]})\n")
+    txt_file.write(f"{candidate_selection[0][1][0]}: {holder_two}% ({candidate_selection[0][1][1]})\n")
+    txt_file.write(f"{candidate_selection[0][2][0]}: {holder_three}% ({candidate_selection[0][2][1]})\n")
+    txt_file.write(f"{candidate_selection[0][3][0]}: {holder_four}% ({candidate_selection[0][3][1]})\n")
+    txt_file.write("-------------------------\n")
+    txt_file.write(f"Winner:  {candidate_selection[0][0][0]}\n")
+    txt_file.write("-------------------------\n")   
+
+# -->>  Export results to text file 
 # election_file = os.path.join("Output", "outputpypoll.txt")
 # with open(outputpypoll.txt, "w") as txt_file:
 
-    # txt_file.write("Election Results\n")
-    # txt_file.write("-------------------------\n")
-    # txt_file.write(f"Total Votes:  {sum(candidate_count.values())}\n")
-    # txt_file.write("-------------------------\n")
-    # txt_file.write(f"{candidate_selection[0][0][0]}: {holder_one}% ({candidate_selection[0][0][1]})\n")
-    # txt_file.write(f"{candidate_selection[0][1][0]}: {holder_two}% ({candidate_selection[0][1][1]})\n")
-    # txt_file.write(f"{candidate_selection[0][2][0]}: {holder_three}% ({candidate_selection[0][2][1]})\n")
-    # txt_file.write(f"{candidate_selection[0][3][0]}: {holder_four}% ({candidate_selection[0][3][1]})\n")
-    # txt_file.write("-------------------------\n")
-    # txt_file.write(f"Winner:  {candidate_selection[0][0][0]}\n")
-    # txt_file.write("-------------------------\n")   
+# -->>  Export a text file with the results
+# election_file = os.path.join("outputpypoll.txt")
+# with open(election_file, "w") as outfile:
+
+
+#     outfile.write("Election Results\n")
+#     outfile.write("-------------------------\n")
+#     outfile.write(f"Total Votes:  {sum(candidate_count.values())}\n")
+#     outfile.write("-------------------------\n")
+#     outfile.write(f"{candidate_selection[0][0][0]}: {holder_one}% ({candidate_selection[0][0][1]})\n")
+#     outfile.write(f"{candidate_selection[0][1][0]}: {holder_two}% ({candidate_selection[0][1][1]})\n")
+#     outfile.write(f"{candidate_selection[0][2][0]}: {holder_three}% ({candidate_selection[0][2][1]})\n")
+#     outfile.write(f"{candidate_selection[0][3][0]}: {holder_four}% ({candidate_selection[0][3][1]})\n")
+#     outfile.write("-------------------------\n")
+#     outfile.write(f"Winner:  {candidate_selection[0][0][0]}\n")
+#     outfile.write("-------------------------\n")    
